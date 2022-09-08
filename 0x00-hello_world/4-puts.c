07d038ee0bd4020ab2d8fd4514155c55c3398366
Stegeek <stevegach968@gmail.com>
2022-09-08 16:37:52 -0700

    Added files for C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *Using puts function to print a statement
+ *
+ *main(void)-it does not have an argument hence the void
+ *
+ * Return (0)- end with the number 0
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
+
