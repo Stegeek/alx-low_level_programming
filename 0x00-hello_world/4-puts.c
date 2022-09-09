207204c1ca128e7f0c4dc26178b0673cebc78826
Stegeek <stevegach968@gmail.com>
2022-09-08 17:59:28 -0700

    Added three C-files

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
- *Using puts function to print a statement
+ *main - it does not have an argument hence the void
  *
- *main(void)-it does not have an argument hence the void
+ * Description: prints the statement given with quotes inclusive
  *
- * Return (0)- end with the number 0
+ * Return: end with the number 0
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\"\n");
 	return (0);
 }
 
