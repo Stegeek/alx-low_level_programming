0ee4cf673808532db6da0373d68d35c7c3715ecb
Stegeek <stevegach968@gmail.com>
2022-09-09 17:29:17 -0700

    Added a newfile 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Description: Prints all the alphabet except q and e
+ *
+ *Return: returns nothing
+ *
+ */
+
+int main(void)
+{
+	char alphabet = 'a'
+
+	for (alphabet <= 'z'; alphabet != 'e'; alphabet != 'q'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+	putchar('\n')
+	return (0)
+}
